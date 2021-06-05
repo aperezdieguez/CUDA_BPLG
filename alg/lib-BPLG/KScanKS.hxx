afa7b56d3214dc61def5777f02a1488adc83c82b
aperezdieguez <65471404+aperezdieguez@users.noreply.github.com>
2021-06-05 09:53:00 +0200

    Add files via upload

---
+++ alg/lib-BPLG/KScanKS.hxx
@@ -0,0 +1,22 @@
+//- =======================================================================
+//+ Legacy Scan KS 
+//- =======================================================================
+
+
+#pragma once
+#ifndef KScanKS_HXX
+#define KScanKS_HXX
+
+//---- Header Dependencies -----------------------------------------------
+
+#include "tools/cuvector.hxx"
+
+//---- Function Declaration ----------------------------------------------
+
+
+int KScanKS(float* data,float* dataOut, int dir, int N, int M, int batch);
+
+
+
+#endif // KScanKS_HXX
+
